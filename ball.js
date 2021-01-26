80aacde4b57c57e73e1663d1b0d60b71b3daa0d7
Ashiwashi <72352700+Ashiwashi@users.noreply.github.com>
2021-01-25 21:52:27 -0600

    Add files via upload

--- ball.js
+++ ball.js
@@ -1,25 +1,10 @@
-class Ball {
-    constructor(x,y,radius) {
-      var options = {
-          restitution:0.3,
-          friction:0.5,
-          density:1.2
+class Ball extends BaseClass{
+        constructor(x,y){
+        super(x,y,120,100);
+        this.image = loadImage("Steelball.jpg");
 
-      }
-      this.body = Matter.Bodies.circle(x,y,radius,options);
-      this.radius = radius;
-      this.image = loadImage("paper.png");
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-      //ellipseMode(CENTER);
-      fill("magenta");
-      push();
-      translate(this.body.position.x, this.body.position.y);
-      rotate(this.body.angle);
-      image(this.image, 0, 0, this.radius);
-      pop();
-    }
     
-  };
+    }
+
+
+}
